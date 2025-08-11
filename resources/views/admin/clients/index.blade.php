afe07ea6acfe8118e8dcd3af19688b0d3f91d52b
AndreaWeb01 <andreawebfac@gmail.com>
2025-08-11 12:12:14 +0000

    Mise a jour produits et clients

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -54,48 +54,7 @@
                         @endforeach
                     </tbody>
                 </table>
-    <table class="table table-hover table-bordered dt-responsive nowrap w-100">
-        <thead class="table-dark">
-            <tr class="align-item-center">
-                <th>Code Client</th>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Téléphone</th>
-                <th>Adresse</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($clients as $client)
-                <tr>
-                    <td>{{ $client->code_client }}</td>
-                    <td>{{ $client->nom }}</td>
-                    <td>{{ $client->prenom }}</td>
-                    <td>{{ $client->telephone }}</td>
-                    <td>{{ $client->adresse }}</td>
-                    <td>
-                        @can('gérer client')
-                        <div class="btn-group gap-2">
-                            <a href="{{ route('clients.show', $client) }}" class="btn btn-sm btn-header1 rounded-3">
-                                <i class="fas fa-eye"></i>
-                            </a>
-                            <a href="{{ route('clients.edit', $client) }}" class="btn btn-sm btn-success rounded-3">
-                                <i class="fas fa-edit"></i>
-                            </a>
-                            <form id="delete-form-{{ $client->id }}" action="{{ route('clients.destroy', $client) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                                <button type="button" class="btn btn-sm btn-danger btn-delete rounded-3" data-form-id="delete-form-{{ $client->id }}">
-                                                    <i class="fas fa-trash"></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                               @endcan
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+
 
                 {{ $clients->links() }} <!-- Pagination links -->
             </div> <!-- end card-body -->
