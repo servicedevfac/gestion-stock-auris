db0626c335695c8a4900f6f9cfb44caa80a83b09
Djue Kouassi Celestin <palmer8744@gmail.com>
2026-06-24 15:07:36 +0000

    feat: add password confirmation view for secure authentication access

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -18,10 +18,13 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
+
+
         <div class="flex justify-end mt-4">
             <x-primary-button>
                 {{ __('Confirm') }}
             </x-primary-button>
         </div>
+        
     </form>
 </x-guest-layout>
